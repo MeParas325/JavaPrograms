8bc3f7fd7edbe2568ec91d6aa1e371d7b9d31059
MeParas325 <adamchacha398@gmail.com>
2021-12-15 08:22:19 +0530

    Added more Programs Of applet

---
+++ JavaGUI/JavaApplet/Program7/MyApplet7.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.*;
+/*<applet code="MyApplet7" width="300" height="300"></applet>*/
+public class MyApplet7 extends Applet{
+      public void paint(Graphics g){
+          g.drawString("Welcome gyus chai pee lo", 150, 150);
+      }
+}
