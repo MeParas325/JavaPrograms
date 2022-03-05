452b9e1e824a4b5777ff883cb00289ee43367528
MeParas325 <adamchacha398@gmail.com>
2022-03-05 13:14:54 +0530

    Added SOme more programs

---
+++ JavaCompleteRefrence/Chapter 12 Packages and interfaces/CompletePackageAccess/Demo1.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class Demo1 {
+    public static void main(String args[]){
+        Protection p1 = new Protection();
+        Derived d = new Derived();
+        SamePackage sp = new SamePackage();
+    }
+}
---
+++ JavaCompleteRefrence/Chapter 12 Packages and interfaces/CompletePackageAccess/Demo2.java
@@ -0,0 +1,7 @@
+package p2;
+public class Demo2{
+    public static void main(String args[]){
+        Protection2 obj1 = new Protection2();
+        OtherPackage obj2 = new OtherPackage();
+    }
+}
